9c278ae196d4b5be0de789d21da2c2f5d7a4bf2d
frako23 <frako23@gmail.com>
2023-08-22 11:39:25 -0400

    responsive desigadjustents

--- src/components/Resume/ResumeApi.jsx
+++ src/components/Resume/ResumeApi.jsx
@@ -26,7 +26,7 @@ const ResumeApi = [
     {
       id: 4,
       category: "experience",
-      year: "Kentron Soluciones Tecnológicas  (Sep 2023 - Act) ",
+      year: "Kentron Sistemas de Información  (Sep 2023 - Act) ",
       title: "Web Developer",
       desc: "Contrary to popular belief. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
       rate: "4.70/5 ",
@@ -34,7 +34,7 @@ const ResumeApi = [
     {
       id: 5,
       category: "experience",
-      year: "Job at Rainbow-Themes (2008 - 2016) ",
+      year: "Movistar (2013 - 2016) ",
       title: "The Personal Portfolio Mystery",
       desc: "Generate Lorem Ipsum which looks. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
       rate: "4.95/5 ",
