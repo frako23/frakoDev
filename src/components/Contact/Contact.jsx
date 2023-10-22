bf31fd5c0e66ff334e71d598a7c7b55386837394
frako23 <frako23@gmail.com>
2023-10-22 10:07:24 -0400

    se agrega funcionalidad al formulario

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,106 +1,106 @@
-import React, { useState } from "react"
-// import contact1 from "./contact1.png"
-import "./Contact.css"
+import { useForm, ValidationError } from "@formspree/react";
+import "./Contact.css";
+import Swal from 'sweetalert2'
 
 const Contact = () => {
-  const [data, setData] = useState({
-    fullname: "",
-    phone: "",
-    email: "",
-    subject: "",
-    message: "",})
+  const [state, handleSubmit] = useForm("myyqokwq");
 
-  const InputEvent = (event) => {
-    const { name, value } = event.target
-
-    setData((preVal) => {
-      return {
-        ...preVal,
-        [name]: value,
-      }
-    })
+  if (state.succeeded) {
+    Swal.fire({
+      icon: "success",
+      title: "Thanks for getting in touch with me!! 🙌",
+      text: "I´ll answer ASAP!",
+      confirmButtonColor: "#35AD94",
+      footer: "Sincerely: frakoDev <span>&#128526</span> ",
+    });
+    document.getElementById("contact-form").reset();
   }
 
-  const formSubmit = (event) => {
-    event.preventDefault()
-    alert(
-      `My name is ${data.fullname}. 
-	My phone number is ${data.phone}. 
-	My email address is ${data.email}. 
-	My Subject on  ${data.subject}. 
-	Here is my message I want to say : ${data.message}. 
-	`
-    )
-  }
+
   return (
     <>
-      <section className='Contact' id='contact'>
-        <div className='container top'>
-          <div className='heading text-end'>
+      <section className="Contact" id="contact">
+        <div className="container top">
+          <div className="heading text-end">
             <h4>CONTACT</h4>
             <h1>Contact With Me</h1>
           </div>
 
-          <div className='content d_flex'>
-            <div className='left'>
-              <div className='box box_shodow'>
-                <div className='img'>
-                  <img src="" alt='' />
+          <div className="content d_flex">
+            <div className="left">
+              <div className="box box_shodow">
+                <div className="img">
+                  <img src="" alt="" />
                 </div>
-                <div className='details'>
+                <div className="details">
                   <h1>Francisco Orozco</h1>
                   <p>Software Developer</p>
-                  <p>I am available for freelance work. Connect with me via email or thru call.</p> <br />
+                  <p>
+                    I am available for freelance work. Connect with me via email
+                    or thru call.
+                  </p>{" "}
+                  <br />
                   <p>Phone: +58 04242526757</p>
                   <p>Email: frako23@gmail.com</p> <br />
                   <span>FIND WITH ME</span>
-                  <div className='button f_flex'>
+                  <div className="button f_flex">
                     <a href="mailto:frako23@gmail.com">
-                    <button className='btn_shadow'>
-                      <i className='fa-solid fa-envelope'></i>
-                    </button>
+                      <button className="btn_shadow">
+                        <i className="fa-solid fa-envelope"></i>
+                      </button>
                     </a>
                     <a href="https://wa.me/584242526757">
-                    <button className='btn_shadow'>
-                      <i className='fa-brands fa-whatsapp'></i>
-                    </button>
+                      <button className="btn_shadow">
+                        <i className="fa-brands fa-whatsapp"></i>
+                      </button>
                     </a>
                     <a href="tg://resolve?domain=Frako23">
-                    <button className='btn_shadow'>
-                      <i className='fa-solid fa-paper-plane'></i>
-                    </button>
+                      <button className="btn_shadow">
+                        <i className="fa-solid fa-paper-plane"></i>
+                      </button>
                     </a>
                   </div>
                 </div>
               </div>
             </div>
 
-            <div className='right box_shodow'>
-              <form onSubmit={formSubmit}>
-                <div className='f_flex'>
-                  <div className='input row'>
-                    <span>YOUR NAME</span>
-                    <input type='text' name='fullname' value={data.fullname} onChange={InputEvent} />
-                  </div>
-                  <div className='input row'>
-                    <span>PHONE NUMBER </span>
-                    <input type='number' name='phone' value={data.phone} onChange={InputEvent} />
-                  </div>
+            <div className="right box_shodow">
+              <form onSubmit={handleSubmit} id="contact-form">
+                <div className="input">
+                  <span style={{fontWeight: 'bold'}}>YOUR NAME</span>
+                  <input name="name" id="name" type="text" className="input" />
+                  <ValidationError
+                    prefix="Name"
+                    field="name"
+                    errors={state.errors}
+                  />
                 </div>
-                <div className='input'>
-                  <span>EMAIL </span>
-                  <input type='email' name='email' value={data.email} onChange={InputEvent} />
+                <div className="input">
+                  <span style={{fontWeight: 'bold'}}>EMAIL </span>
+                  <input type="email" name="email" id="email" className="input" />
+                  <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                  />
                 </div>
-                <div className='input'>
-                  <span>SUBJECT </span>
-                  <input type='text' name='subject' value={data.subject} onChange={InputEvent} />
+                <div className="input">
+                  <span style={{fontWeight: 'bold'}}>YOUR MESSAGE </span>
+                  <textarea
+                    cols="30"
+                    rows="10"
+                    name="message"
+                    id="message"
+                    className="input"
+                  ></textarea>
+                  <ValidationError
+                    prefix="Message"
+                    field="message"
+                    errors={state.errors}
+                  />
                 </div>
-                <div className='input'>
-                  <span>YOUR MESSAGE </span>
-                  <textarea cols='30' rows='10' name='message' value={data.message} onChange={InputEvent}></textarea>
-                </div>
-                <button className='btn_shadow'>
-                  SEND MESSAGE <i className='fa fa-long-arrow-right'></i>
+                <button className="btn_shadow" type="submit">
+                  SEND MESSAGE <i className="fa fa-long-arrow-right"></i>
                 </button>
               </form>
             </div>
@@ -108,7 +108,7 @@ const Contact = () => {
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
