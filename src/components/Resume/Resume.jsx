e1bb44fd5212ad87874aa35b70d68e6a2d021155
frako23 <frako23@gmail.com>
2024-01-02 12:54:13 -0400

    ajustes de midudev

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,29 +1,37 @@
-import React from "react"
-import "./Resume.css"
-import ResumeApi from "./ResumeApi"
-import Card from "./Card"
+import React from "react";
+import "./Resume.css";
+import ResumeApi from "./ResumeApi";
+import Card from "./Card";
 
 const Resume = () => {
   return (
     <>
-      <section className='Resume' id='resume'>
-        <div className='container top'>
-          <div className='heading text-center'>
-            <h4>7+ YEARS OF EXPERIENCE</h4>
+      <section className="Resume" id="resume">
+        <div className="container top">
+          <div className="heading text-center">
+            <h4>1+ YEAR OF EXPERIENCE IN CODING</h4>
             <h1>My Resume</h1>
           </div>
 
-          <div className='content-section mtop d_flex'>
-            <div className='left'>
-              <div className='heading'>
+          <div className="content-section mtop d_flex">
+            <div className="left">
+              <div className="heading">
                 <h4>2003-2022</h4>
                 <h1>Education Quality</h1>
               </div>
 
-              <div className='content'>
+              <div className="content">
                 {ResumeApi.map((val, id) => {
                   if (val.category === "education") {
-                    return <Card key={id} title={val.title} year={val.year} rate={val.rate} desc={val.desc} />
+                    return (
+                      <Card
+                        key={id}
+                        title={val.title}
+                        year={val.year}
+                        rate={val.rate}
+                        desc={val.desc}
+                      />
+                    );
                   }
                 })}
 
@@ -42,16 +50,24 @@ const Resume = () => {
                 </div>*/}
               </div>
             </div>
-            <div className='left'>
-              <div className='heading'>
+            <div className="left">
+              <div className="heading">
                 <h4>2009-2023</h4>
                 <h1>Job Experience</h1>
               </div>
 
-              <div className='content'>
+              <div className="content">
                 {ResumeApi.map((val, index) => {
                   if (val.category === "experience") {
-                    return <Card key={index} title={val.title} year={val.year} rate={val.rate} desc={val.desc} />
+                    return (
+                      <Card
+                        key={index}
+                        title={val.title}
+                        year={val.year}
+                        rate={val.rate}
+                        desc={val.desc}
+                      />
+                    );
                   }
                 })}
               </div>
@@ -60,7 +76,7 @@ const Resume = () => {
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;
