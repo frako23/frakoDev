a4eb0b1482b3e98a63725157d9f1bd6db87434df
frako23 <frako23@gmail.com>
2024-03-12 17:42:04 -0400

    ajusta media queries y  cambia texto en education

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -17,7 +17,7 @@ const Resume = () => {
             <div className="left">
               <div className="heading">
                 <h4>2003-2022</h4>
-                <h1>Education Quality</h1>
+                <h1>Education</h1>
               </div>
 
               <div className="content">
