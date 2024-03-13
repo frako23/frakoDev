22866980c06656afad2246ec5e775c5f8215889c
frako23 <frako23@gmail.com>
2024-03-13 06:46:45 -0400

    cambia formato de imagen e imagenes para mostrar

--- src/components/Hero/Home.jsx
+++ src/components/Hero/Home.jsx
@@ -1,78 +1,95 @@
-import React from "react"
-import "./Home.css"
-import hero from "../../assets/images/foto.png"
+import React from "react";
+import "./Home.css";
+// import hero from "../../assets/images/foto.png"
+import avif from "../../assets/images/fotoAvif.avif";
 // import skill1 from "../pic/skill1.png"
 // import skill2 from "../pic/skill2.png"
 // import skill3 from "../pic/skill3.png"
-import { Typewriter } from "react-simple-typewriter"
+import { Typewriter } from "react-simple-typewriter";
 
 const Home = () => {
   return (
     <>
-      <section className='hero' id='home'>
-        <div className='container f_flex top'>
-          <div className='left top'>
+      <section className="hero" id="home">
+        <div className="container f_flex top">
+          <div className="left top">
             {/* <h3>WELCOME TO MY WORLD</h3> */}
-            <h1 style={{color:"white"}}>
+            <h1 style={{ color: "white" }}>
               Hi, I’m <span>Francisco</span>,
             </h1>
             <h2>
               I'm a
               <span>
-                <Typewriter words={[" Marketing Specialist.", " Developer.", " Geek.", " Coding passionate."]} cursor cursorStyle='|' typeSpeed={70} deleteSpeed={50} delaySpeed={1000} />
+                <Typewriter
+                  words={[
+                    " Marketing Specialist.",
+                    " Developer.",
+                    " Geek.",
+                    " Coding passionate.",
+                  ]}
+                  cursor
+                  cursorStyle="|"
+                  typeSpeed={70}
+                  deleteSpeed={50}
+                  delaySpeed={1000}
+                />
               </span>
             </h2>
 
-            <p>I am a full stack developer with more than 13 years of experience in marketing and sales who decided to make a 180 degree change in his professional life to pursue his passion for working in technology.</p>
+            <p>
+              I am a full stack developer with more than 13 years of experience
+              in marketing and sales who decided to make a 180 degree change in
+              his professional life to pursue his passion for working in
+              technology.
+            </p>
 
-            <div className='hero_btn d_flex'>
-              <div className='col_1'>
+            <div className="hero_btn d_flex">
+              <div className="col_1">
                 <h4>FIND ME IN</h4>
-                <div className='button'>
+                <div className="button">
                   <a href="https://github.com/frako23">
-                  <button className='btn_brands'>
-                    <i className='fab fa-github'></i>
-                  </button>
+                    <button className="btn_brands">
+                      <i className="fab fa-github"></i>
+                    </button>
                   </a>
                   <a href="https://www.linkedin.com/in/franciscorozco/?locale=en_US">
-                  <button className='btn_brands'>
-                  <i className='fab fa-linkedin-in'></i>
-                  </button>
+                    <button className="btn_brands">
+                      <i className="fab fa-linkedin-in"></i>
+                    </button>
                   </a>
                   <a href="https://twitter.com/frakoDev">
-                  <button className='btn_brands'>
-                    <i className="fa-brands fa-x-twitter"></i>
-                  </button>
+                    <button className="btn_brands">
+                      <i className="fa-brands fa-x-twitter"></i>
+                    </button>
                   </a>
                 </div>
               </div>
-              <div className='col_1'>
+              <div className="col_1">
                 <h4>BEST SKILLS ON</h4>
-                <button className='btn_brands'>
-                <i className="fa-brands fa-square-js"></i>
+                <button className="btn_brands">
+                  <i className="fa-brands fa-square-js"></i>
                 </button>
-                <button className='btn_brands'>
-                <i className="fa-brands fa-react"></i>
+                <button className="btn_brands">
+                  <i className="fa-brands fa-react"></i>
                 </button>
-                <button className='btn_brands'>
-                <i className="fa-brands fa-python"></i>
+                <button className="btn_brands">
+                  <i className="fa-brands fa-python"></i>
                 </button>
-                <button className='btn_brands'>
-                <i className="fa-brands fa-git-alt"></i>
+                <button className="btn_brands">
+                  <i className="fa-brands fa-git-alt"></i>
                 </button>
-
               </div>
             </div>
           </div>
-          <div className='right'>
-            <div className='right_img'>
-              <img src={hero} alt='' />
+          <div className="right">
+            <div className="right_img">
+              <img src={avif} alt="" />
             </div>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Portfolio/Portfolio_data.jsx
+++ src/components/Portfolio/Portfolio_data.jsx
@@ -28,12 +28,12 @@ const Portfolio_data = [
       "- Completly reponsive",
       "- With form contact section",
     ],
-    proyectUrl: "https://tuplanificadorafinanciera.com/",
+    proyectUrl: "https://tu-plan-fpx7y3mry-frako23.vercel.app/",
   },
   {
     id: 3,
-    category: "WEB MOBILE E-COMMERCE APP",
-    technologies: ["JavaScript", "React", "CSS"],
+    category: "medical uniform store web catalog",
+    technologies: ["Astro", "Tailwind", "React"],
     title: "Brakes selling app",
     image:
       "https://raw.githubusercontent.com/frako23/frakoDev/main/src/components/Portfolio/img/brakes.png",
@@ -46,56 +46,56 @@ const Portfolio_data = [
     ],
     proyectUrl: "https://test-catalog-eight.vercel.app/",
   },
-  {
-    id: 4,
-    category: "WEB PAGE",
-    technologies: ["JavaScript", "React", "CSS"],
-    title: "Devix",
-    image:
-      "https://raw.githubusercontent.com/frako23/frakoDev/main/src/components/Portfolio/img/devix.png",
-    description: [
-      "- App oriented to enhance the performance of the sales force",
-      "- Contains a training section with videos and a comments and answers section",
-      "- CRM platform with sales funnel (Prospects / Contacted / First Appointment / Negotiation / Closing)",
-      "- Section to register activities with clients (Calls, Sending text messages / Emails, Meetings)",
-      "- Section to register pending tasks with the Kanban methodology (To Do / In process / Done)",
-      "- Manager view to access the CRM and activities with clients of each of the members of your team",
-    ],
-    proyectUrl: "https://www.linkedteam.app/",
-  },
-  {
-    id: 5,
-    category: "E-COMMERCE",
-    technologies: ["JavaScript", "React", "CSS"],
-    title: "Droguería Meraki",
-    image:
-      "https://raw.githubusercontent.com/frako23/frakoDev/main/src/components/Portfolio/img/meraki.png",
-    description: [
-      "- App oriented to enhance the performance of the sales force",
-      "- Contains a training section with videos and a comments and answers section",
-      "- CRM platform with sales funnel (Prospects / Contacted / First Appointment / Negotiation / Closing)",
-      "- Section to register activities with clients (Calls, Sending text messages / Emails, Meetings)",
-      "- Section to register pending tasks with the Kanban methodology (To Do / In process / Done)",
-      "- Manager view to access the CRM and activities with clients of each of the members of your team",
-    ],
-    proyectUrl: "https://www.linkedteam.app/",
-  },
-  {
-    id: 6,
-    category: "API INTEGRATION",
-    technologies: ["JavaScript", "React", "CSS"],
-    title: "Adventure Travels",
-    image:
-      "https://raw.githubusercontent.com/frako23/frakoDev/main/src/components/Portfolio/img/adventure-travels.png",
-    description: [
-      "- App oriented to enhance the performance of the sales force",
-      "- Contains a training section with videos and a comments and answers section",
-      "- CRM platform with sales funnel (Prospects / Contacted / First Appointment / Negotiation / Closing)",
-      "- Section to register activities with clients (Calls, Sending text messages / Emails, Meetings)",
-      "- Section to register pending tasks with the Kanban methodology (To Do / In process / Done)",
-      "- Manager view to access the CRM and activities with clients of each of the members of your team",
-    ],
-    proyectUrl: "https://www.linkedteam.app/",
-  },
+  // {
+  //   id: 4,
+  //   category: "WEB PAGE",
+  //   technologies: ["JavaScript", "React", "CSS"],
+  //   title: "Devix",
+  //   image:
+  //     "https://raw.githubusercontent.com/frako23/frakoDev/main/src/components/Portfolio/img/devix.png",
+  //   description: [
+  //     "- App oriented to enhance the performance of the sales force",
+  //     "- Contains a training section with videos and a comments and answers section",
+  //     "- CRM platform with sales funnel (Prospects / Contacted / First Appointment / Negotiation / Closing)",
+  //     "- Section to register activities with clients (Calls, Sending text messages / Emails, Meetings)",
+  //     "- Section to register pending tasks with the Kanban methodology (To Do / In process / Done)",
+  //     "- Manager view to access the CRM and activities with clients of each of the members of your team",
+  //   ],
+  //   proyectUrl: "https://www.linkedteam.app/",
+  // },
+  // {
+  //   id: 5,
+  //   category: "E-COMMERCE",
+  //   technologies: ["JavaScript", "React", "CSS"],
+  //   title: "Droguería Meraki",
+  //   image:
+  //     "https://raw.githubusercontent.com/frako23/frakoDev/main/src/components/Portfolio/img/meraki.png",
+  //   description: [
+  //     "- App oriented to enhance the performance of the sales force",
+  //     "- Contains a training section with videos and a comments and answers section",
+  //     "- CRM platform with sales funnel (Prospects / Contacted / First Appointment / Negotiation / Closing)",
+  //     "- Section to register activities with clients (Calls, Sending text messages / Emails, Meetings)",
+  //     "- Section to register pending tasks with the Kanban methodology (To Do / In process / Done)",
+  //     "- Manager view to access the CRM and activities with clients of each of the members of your team",
+  //   ],
+  //   proyectUrl: "https://www.linkedteam.app/",
+  // },
+  // {
+  //   id: 6,
+  //   category: "API INTEGRATION",
+  //   technologies: ["JavaScript", "React", "CSS"],
+  //   title: "Adventure Travels",
+  //   image:
+  //     "https://raw.githubusercontent.com/frako23/frakoDev/main/src/components/Portfolio/img/adventure-travels.png",
+  //   description: [
+  //     "- App oriented to enhance the performance of the sales force",
+  //     "- Contains a training section with videos and a comments and answers section",
+  //     "- CRM platform with sales funnel (Prospects / Contacted / First Appointment / Negotiation / Closing)",
+  //     "- Section to register activities with clients (Calls, Sending text messages / Emails, Meetings)",
+  //     "- Section to register pending tasks with the Kanban methodology (To Do / In process / Done)",
+  //     "- Manager view to access the CRM and activities with clients of each of the members of your team",
+  //   ],
+  //   proyectUrl: "https://www.linkedteam.app/",
+  // },
 ];
 export default Portfolio_data;
