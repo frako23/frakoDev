be9fbce3278d158599bb76ef385988feec7ecd7c
frako23 <frako23@gmail.com>
2024-03-13 17:41:01 -0400

    elimina carga innecesaria de fuentes y corrige descripcion en open graph

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Header from "./components/Head/Header";
-import Features from "./components/Features/Features";
+// import Features from "./components/Features/Features";
 import Home from "./components/Hero/Home";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Resume from "./components/Resume/Resume";
-import Testimonial from "./components/Testimonial/Testimonial";
+// import Testimonial from "./components/Testimonial/Testimonial";
 // import Blog from "./components/Blog/Blog"
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer";
